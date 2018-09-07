1ed4e070446661c595ba68fd877d62aac3a654e4
alasprilla <43018352+alasprilla@users.noreply.github.com>
2018-09-07 08:15:16 -0500

    desarrollo

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/definition/DefPagaTuFacturaMovil.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/definition/DefPagaTuFacturaMovil.java
@@ -18,23 +18,27 @@ public class DefPagaTuFacturaMovil {
 
 	@When("^ingreso el número celular \"([^\"]*)\" en el campo \"([^\"]*)\"$")
 	public void ingreso_el_número_celular_en_el_campo(String numcelular) {
-		pagePagaFactMvl.EscrbirCelular(numcelular);
+		pagePagaFactMvl.escribirCelular(numcelular);
 	}
 
 	@When("^ingreso el correo \"([^\"]*)\" en el campo \"([^\"]*)\"$")
-	public void ingreso_el_correo_en_el_campo(String arg1, String arg2)  {
+	public void ingreso_el_correo_en_el_campo(String correo)  {
+		pagePagaFactMvl.escribirCorreo(correo);
 	}
 
 	@Then("^mostrará el siguiente mensaje en letras blancas con fondo rojo \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
-	public void mostrará_el_siguiente_mensaje_en_letras_blancas_con_fondo_rojo(String arg1, String arg2, String arg3) {
+	public void mostrará_el_siguiente_mensaje_en_letras_blancas_con_fondo_rojo(String mensaje) {
+		pagePagaFactMvl.mostrarMensaje();
 	}
 
 	@Then("^mostrará el siguiente mensaje en letras blancas con fondo azul \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
-	public void mostrará_el_siguiente_mensaje_en_letras_blancas_con_fondo_azul(String arg1, String arg2, String arg3) {
+	public void mostrará_el_siguiente_mensaje_en_letras_blancas_con_fondo_azul(String mensaje) {
+		pagePagaFactMvl.mostrarMensaje();
 	}
 
 	@Then("^llevará al formulario donde se selecciona el tipo de pago$")
 	public void llevará_al_formulario_donde_se_selecciona_el_tipo_de_pago()  {
+		pagePagaFactMvl.presionBotonConfirmar();
 	}
 
 }
--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/pageobjects/PagePagaTuFacturaMovil.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/pageobjects/PagePagaTuFacturaMovil.java
@@ -1,16 +1,38 @@
 package com.handresc1127.automatizacion.pageobjects;
 
+import org.openqa.selenium.By;
+
 import com.handresc1127.automatizacion.utilities.actionsUtil;
 
 import net.serenitybdd.core.pages.PageObject;
 
 public class PagePagaTuFacturaMovil extends PageObject{
+	public By txtMsisdn= By.id("edit-candidate-number");
+	public By btnConfirmar= By.xpath("//*[@id='edit-button-query']");
+	public By txtCorreo= By.id("edit-email");
+	String mensaje="No hemos encontrado facturas para este número de línea";
 	
 	
-	public void irAlaPagina() {
-		actionsUtil.goToWebSide(getDriver(), "https://transaccionesco-uat.tigocloud.net/servicios/factura-movil");
+	public void irAlaPagina(String url) {
+		actionsUtil.goToWebSide(getDriver(), url);
+	}
+
+	public void escribirCelular(String numcelular) {
+		actionsUtil.setTextFieldSlowly(getDriver(), txtMsisdn, numcelular);
+		
+	}
+	public void escribirCorreo(String correo) {
+		actionsUtil.setTextFieldSlowly(getDriver(), txtCorreo, correo);
+	}
+	
+	public void presionBotonConfirmar() {
+		actionsUtil.clic(getDriver(), btnConfirmar);
+	}
+	
+	public void mostrarMensaje() {
+		actionsUtil.clic(getDriver(), mensaje);
 	}
 
 
-
+	
 }
