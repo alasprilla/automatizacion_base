00a4024f789f31c77618850d66eff37a1a84dddb
Handresc1127 <h.andresc1127@hotmail.com>
2018-09-20 11:42:45 -0500

    CasoFeliz

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/RunnerTags.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/RunnerTags.java
@@ -20,7 +20,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 		},
 
 
-		tags = {}, monochrome = true, plugin = { "pretty", "html:target/reports/html/",
+		tags = {"@CasoFeliz"}, monochrome = true, plugin = { "pretty", "html:target/reports/html/",
 				"junit:target/reports/junit/allcukes.xml", "json:target/reports/cukes.json" })
 
 public class RunnerTags {
