b1d4c3ff3d7e82885238291196dd1dc274edb2b5
Handresc1127 <h.andresc1127@hotmail.com>
2018-10-04 10:05:38 -0500

    publicacionEvidencias.sh funcional linux

--- publicacionEvidencias.sh
+++ publicacionEvidencias.sh
@@ -1,3 +1,6 @@
-ssh -i ..\\..\\Metis-WebReports.ppk metis@190.248.48.5 'rm -rf /var/www/metis/atdd/*'
-echo "put -r automatizacion_base/target/site/serenity/* /var/www/metis/atdd/" | sftp -i ..\\..\\Metis-WebReports.ppk metis@190.248.48.5
-ssh -i ..\\..\\Metis-WebReports.ppk metis@190.248.48.5 'convmv -r -f cp1252 -t utf-8 /var/www/metis/atdd/* --notest > /dev/null'
+#!/bin/bash
+
+chmod 600 ../../Metis-WebReports.priv
+ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/*'
+echo "put -r automatizacion_base/target/site/serenity/* /var/www/metis/atdd/" | sftp -i ../../Metis-WebReports.priv metis@190.248.48.5
+ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'convmv -r -f cp1252 -t utf-8 /var/www/metis/atdd/* --notest > /dev/null'
