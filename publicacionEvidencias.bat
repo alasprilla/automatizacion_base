f158201b74b56316f5522373eaef26f787158d2a
Handresc1127 <h.andresc1127@hotmail.com>
2018-10-04 17:13:51 -0500

    publicacionEvidencias.bat funcionando en windows
    Se debe de terner las 2 llaves en la raiz de jenkins

--- publicacionEvidencias.bat
+++ publicacionEvidencias.bat
@@ -1,2 +1,10 @@
+set key="..\\..\\Metis-WebReports.priv"
+cmd /c icacls %key% /c /t /inheritance:d
+cmd /c icacls %key% /c /t /grant %username%:F
+cmd /c icacls %key%  /c /t /remove Administrator BUILTIN\Administrators BUILTIN Everyone System Users
+cmd /c icacls %key%
+
+ssh -i ..\\..\\Metis-WebReports.priv metis@190.248.48.5 "rm -rf /var/www/metis/atdd/*"
 echo y|pscp -r -i ..\\..\\Metis-WebReports.ppk "automatizacion_base\target\site\serenity\*" metis@190.248.48.5:/var/www/metis/atdd/
-curl http://190.248.48.5/metis/command.php?rename
+ssh -i ..\\..\\Metis-WebReports.priv metis@190.248.48.5 "convmv -r -f cp1252 -t utf-8 /var/www/metis/atdd/* --notest > /dev/null"
+
