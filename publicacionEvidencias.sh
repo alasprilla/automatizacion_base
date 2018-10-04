587aa3d6fcd8e579d769934fe6d354c45c1466fb
Handresc1127 <h.andresc1127@hotmail.com>
2018-10-04 09:42:08 -0500

    Publicacion de evidencias sh

--- publicacionEvidencias.sh
+++ publicacionEvidencias.sh
@@ -1,2 +1,3 @@
-scp -r -i ..\\..\\Metis-WebReports.ppk "automatizacion_base\target\site\serenity\*" metis@190.248.48.5:/var/www/metis/atdd/
-curl http://190.248.48.5/metis/command.php?rename
+ssh -i ..\\..\\Metis-WebReports.ppk metis@190.248.48.5 'rm -rf /var/www/metis/atdd/*'
+echo "put -r automatizacion_base/target/site/serenity/* /var/www/metis/atdd/" | sftp -i ..\\..\\Metis-WebReports.ppk metis@190.248.48.5
+ssh -i ..\\..\\Metis-WebReports.ppk metis@190.248.48.5 'convmv -r -f cp1252 -t utf-8 /var/www/metis/atdd/* --notest > /dev/null'
