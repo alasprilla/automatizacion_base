91fcf5d4fb9f0fd4f0bfc2ef5b6a3f56a053692b
alasprilla <lasprilla.alexandra@gmail.com>
2018-10-19 15:13:55 -0500

    Desarrollo de la HU016 de Recargas y Paquetes

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/definition/DefRecargasPaquetes.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/definition/DefRecargasPaquetes.java
@@ -1,27 +1,34 @@
 package com.handresc1127.automatizacion.definition;
 
+import com.handresc1127.automatizacion.pageobjects.PageRecargasPaquetes;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class DefRecargasPaquetes {
 	
-	@Given("^Estoy en la página de inicio de compra paquetes y recargas \"([^\"]*)\"$")
-	public void estoy_en_la_página_de_inicio_de_compra_paquetes_y_recargas(String arg1) {
+	PageRecargasPaquetes pageRecargasPaquetes;
 	
+	@Given("^Estoy en la página de inicio de compra paquetes y recargas \"([^\"]*)\"$")
+	public void estoy_en_la_página_de_inicio_de_compra_paquetes_y_recargas(String url) {
+		pageRecargasPaquetes.open();
+		pageRecargasPaquetes.irPagina(url);
 	}
 	
 	@When("^Doy clic en \"([^\"]*)\"$")
-	public void doy_clic_en(String arg1) {
+	public void doy_clic_en(String objeto) {
+		pageRecargasPaquetes.clic(objeto);
 	}
 
-
 	@When("^presiono la tecla \"([^\"]*)\"$")
-	public void presiono_la_tecla(String arg1) {
+	public void presiono_la_tecla(String tecla) {
+		pageRecargasPaquetes.presionarTecla(tecla);
 	}
 
 	@Then("^la página se recarga$")
 	public void la_página_se_recarga() {
+		pageRecargasPaquetes.validarPagRecargada();
 	}
 
 
--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/pageobjects/PageRecargasPaquetes.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/pageobjects/PageRecargasPaquetes.java
@@ -2,13 +2,69 @@ package com.handresc1127.automatizacion.pageobjects;
 
 import org.openqa.selenium.By;
 
+import com.handresc1127.automatizacion.negocio.BussinesUtil;
+import com.handresc1127.automatizacion.utilities.ActionsUtil;
+
+import net.serenitybdd.core.pages.PageObject;
+
 import net.thucydides.core.annotations.DefaultUrl;
+
+import java.util.Dictionary;
+
 
 @DefaultUrl("https://www.google.com.co")
 
-public class PageRecargasPaquetes {
+public class PageRecargasPaquetes extends PageObject  {
 	
+	
+//Elemetos de la sección de Recargas	
 	By btnRecarga=By.xpath("//*[@id='tap_selector']/div[2]");
+	By txtTuLineaTigoR=By.xpath("//*[@id='num_lin']");
 
+	By objetoToAction;
+
+	public void irPagina(String url) {
+		ActionsUtil.goToWebSide(getDriver(), url);
+	}
+	
+	public void sharedObjet(String opcion) {
+		
+		ObjetosPagaTuFactura x = new ObjetosPagaTuFactura();
+		String nombreObjeto = (ActionsUtil.textoMinusculasSinEspacios(opcion));
+		By byObjeto = x.getObjetoPagaTuFactura(nombreObjeto);
+		setObjetoToCliked(byObjeto);	
+
+	}
+	
+	public void sharedObjet(Dictionary<String, By> objetos,String opcion) {
+		String nombreObjeto = (ActionsUtil.textoMinusculasSinEspacios(opcion));
+		By byObjetos = objetos.get(nombreObjeto);
+		setObjetoToCliked(byObjetos);
+	}
+	public By getObjetoToCliked() {
+		return objetoToAction;
+	}
+
+	public void setObjetoToCliked(By objetoToCliked) {
+		this.objetoToAction = objetoToCliked;
+	}
+	
+	public void clic(Dictionary<String, By> objetos,String objeto) {
+		sharedObjet(objetos,objeto);
+		ActionsUtil.clic(getDriver(), getObjetoToCliked());
+	}
+	public void clic(String objeto) {
+		sharedObjet(objeto);
+		ActionsUtil.clic(getDriver(), getObjetoToCliked());
+	}
+
+	
+	public void presionarTecla(String tecla) {
+		ActionsUtil.presionarTecla(getDriver(), getObjetoToCliked(), tecla);
+	}
+	
+	public void validarPagRecargada() {
+		ActionsUtil.highlightElement(getDriver(), getObjetoToCliked());
+	}
 
 }
