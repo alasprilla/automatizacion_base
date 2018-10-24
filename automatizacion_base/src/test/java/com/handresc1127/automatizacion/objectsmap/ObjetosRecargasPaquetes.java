9820b261ae1141363aecf179dd31d345e7bfb0b9
nelsonmesa1980 <44276145+nelsonmesa1980@users.noreply.github.com>
2018-10-24 16:53:05 -0500

    Se añede HU_20

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/ObjetosRecargasPaquetes.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/ObjetosRecargasPaquetes.java
@@ -8,11 +8,13 @@ public class ObjetosRecargasPaquetes {
 	
 	public ObjetosRecargasPaquetes () {
 		ActionsUtil.objetosPut("recarga",By.xpath("//*[@id='tap_selector']/div[2]"));
-		ActionsUtil.objetosPut("ingresatulineatigo", By.id("num_lin"));
-		//ActionsUtil.objetosPut("ingresatulineatigo", By.id("recharge-line"));
-		ActionsUtil.objetosPut("ingresatulineatigo", By.id("num_lin"));
+		//ActionsUtil.objetosPut("ingresatulineatigo", By.id("num_lin"));
+		ActionsUtil.objetosPut("ingresatulineatigo", By.id("recharge-line"));
 		ActionsUtil.objetosPut("correoelectronico" , By.id("edit-email"));
 		ActionsUtil.objetosPut("recargar", By.id("edit-button-recargar"));
+		ActionsUtil.objetosPut("otrovalor", By.xpath("//*[@id='container_recharge']/div[1]/div/div[2]/div/ul/div/li[6]"));
+		ActionsUtil.objetosPut("valorarecargar", By.xpath("//*[@id='edit-otro-valor']"));
+		ActionsUtil.objetosPut("labelerrorvalorrecargas", By.xpath("//*[@id='other_value']/div/div/div[1]/p"));
 		ActionsUtil.objetosPut("labelerrorcelularrecargas",By.xpath("//*[@id='block-tieneinformaciondesaldoydeudadelmsisdn']/div[1]/div/div[1]/div[2]/div[1]/p"));
 	}
 }
