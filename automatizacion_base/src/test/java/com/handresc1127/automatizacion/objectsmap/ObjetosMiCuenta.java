86cd57a20b5dbe4ed4ecc28e4ae5633258cd3833
alasprilla <lasprilla.alexandra@gmail.com>
2018-11-08 17:38:47 -0500

    Se termina el desarrollo de la HU-005 de la página Mi Cuenta

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/ObjetosMiCuenta.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/ObjetosMiCuenta.java
@@ -10,10 +10,16 @@ public class ObjetosMiCuenta {
 		
 		ActionsUtil.objetosPut("registrarme", By.xpath("//*[@id='main-content']/div[2]/div[2]/div/div/div/section/div[3]/a[1]"));
 		ActionsUtil.objetosPut("creatucuenta", By.xpath("/html/body/div[2]/div/div[1]/h5"));
-		ActionsUtil.objetosPut("tipo", By.xpath("//*[@id=\'t_document\']/div/div/input"));
+		ActionsUtil.objetosPut("tipo", By.xpath("//*[@id=\"t_document\"]/div/div/input"));
+		ActionsUtil.objetosPut("tipoul", By.xpath("//*[@id=\'t_document\']/div/div/ul"));
+		ActionsUtil.objetosPut("tipoulcc", By.xpath("//*[@id=\'t_document\']/div/div/ul/li[1]"));
+		ActionsUtil.objetosPut("tipoulce", By.xpath("//*[@id=\'t_document\']/div/div/ul/li[2]"));
+		ActionsUtil.objetosPut("tipoulnit", By.xpath("//*[@id=\'t_document\']/div/div/ul/li[3]"));
+		ActionsUtil.objetosPut("tipoulps", By.xpath("//*[@id=\'t_document\']/div/div/ul/li[4]"));
 		ActionsUtil.objetosPut("ingresatudocumento", By.xpath("//*[@id='document']"));
-		ActionsUtil.objetosPut("consultar", By.xpath("//*[@id=\'findagenda\']/div[2]/div[2]"));
+		ActionsUtil.objetosPut("consultar", By.xpath("//*[@id=\'findagenda\']/div[2]/div[2]/button"));
 		ActionsUtil.objetosPut("labelerrordocumento", By.xpath("//*[@id=\'query-container\']/section/div[1]/span[2]"));
+		ActionsUtil.objetosPut("visitaspendientes", By.xpath("//*[@id=\'schedule-visits-container\']/div[2]/div[1]/div/p"));
 	}
 	
 }
