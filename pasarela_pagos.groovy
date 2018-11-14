520ecccebc6cd999a2e2a69b3b0a1a9c63a6d1f1
hacorrea <hacorrea@indracompany.com>
2018-11-14 18:01:47 -0500

    Se inicia versionamiento del proyecto DSL de pruebas para Jenkins

---
+++ pasarela_pagos.groovy
@@ -0,0 +1,122 @@
+job("Pasarela_pagos") {
+	description("Pruebas automatizadas proyecto de pasarela de pagos.")
+  parameters {
+        stringParam('URL1', 'https://transaccionesco-uat.tigocloud.net', 'URL base para TRANSACCIONES-CO, ingrese la URL del ambiente a probar (stage-uat-producción)')
+    	stringParam('URL2', 'https://tigoselfcareregional-uat-co.tigocloud.net', 'URL base SELFCARE REGIONAL, ingrese la URL del ambiente a probar (stage-uat-producción)')
+        choiceParam('ETIQUETA', ['', '@CasoFeliz', '@Pasarela', '@Recargas', '@Paquetes'],'Se ejecutará solo la etiqueta seleccionada, por defecto se ejecutaran todas las pruebas')
+    	choiceParam('NAVEGADOR', ['chrome', 'firefox', 'iexplorer'],'Navegador de ejecución de pruebas')
+    }
+	keepDependencies(false)
+	multiscm {
+		git {
+			remote {
+				name('metis')
+				url('ssh://git@code.tigocloud.net/diffusion/137/metis.git')
+				credentials("e220c410-6ce3-4c0c-b7bc-1a14eeeb1d6c")
+				branch("*/master")
+              	extensions {
+              		relativeTargetDirectory('metis')
+              	}
+			}
+		}
+		git {
+			remote {
+				name('transaccionales-co')
+				url('ssh://git@code.tigocloud.net/diffusion/TRXCO/transaccionales-co.git')
+				credentials("e220c410-6ce3-4c0c-b7bc-1a14eeeb1d6c")
+				branch("*/master")
+              	extensions {
+              		relativeTargetDirectory('transaccionales-co')
+              	}
+			}
+		}
+	}
+	disabled(false)
+    triggers {
+          githubPush()
+      }
+
+  concurrentBuild(false)
+
+	steps {
+		shell('chmod a+x metis/automatizacion_base/src/test/resources/driver/*')
+    }
+
+	steps {
+		configure {
+			node->
+			node / builders / 'hudson.tasks.Maven' {
+				targets('clean install')
+				mavenName('(Default)')
+				pom('metis/automatizacion_base/')
+				properties("chrome.switches=--headless \nwebdriver.chrome.driver=src/test/resources/driver/chromedriver\ncucumber.options=--tags \${ETIQUETA}\nwebdriver.driver=\${NAVEGADOR}\nwebdriver.remote.driver=\${NAVEGADOR}")
+				usePrivateRepository('false')
+				injectBuildVariables('false')
+			}
+		}
+    }
+	
+	steps {
+      shell('chmod a+x metis/publicacionEvidencias.sh')
+      shell('metis/publicacionEvidencias.sh')
+	  //batchFile("metis/publicacionEvidencias.bat")
+    }
+
+	configure {
+		node->
+		node / builders << 'hudson.plugins.sonar.SonarRunnerBuilder' {
+			project('')
+			properties('project.settings=metis/sonarMetis.properties')
+			javaOpts('')
+			additionalArguments('')
+			jdk('')
+			task('')
+		}
+	}
+		
+	configure {
+		node->
+		node / builders << 'hudson.plugins.sonar.SonarRunnerBuilder' {
+			project('')
+			properties('project.settings=metis/sonarTransaccionales.properties')
+			javaOpts('')
+			additionalArguments('')
+			jdk('')
+			task('')
+		}
+	}
+
+	publishers {
+		configure {
+			node->
+			node / publishers / 'jenkins.plugins.slack.SlackNotifier' {		
+				  baseUrl('https://metisgrupo.slack.com/services/hooks/jenkins-ci/')
+				  teamDomain('')
+				  authToken('OKzRnYp2kaVzfPY8ySNtx0qD')
+				  authTokenCredentialId('')
+				  botUser('false')
+				  room('#General')
+				  startNotification('true')
+				  notifySuccess('true')
+				  notifyAborted('true')
+				  notifyNotBuilt('true')
+				  notifyUnstable('true')
+				  notifyRegression('true')
+				  notifyFailure('true')
+				  notifyBackToNormal('true')
+				  notifyRepeatedFailure('true')
+				  includeTestSummary('true')
+				  includeFailedTests('true')
+				  commitInfoChoice('NONE')
+				  includeCustomMessage('false')
+				  customMessage('hola Metis!')
+			}
+		}
+	
+      	archiveJunit("metis/automatizacion_base/target/reports/junit.xml") {
+			healthScaleFactor(1.0)
+			allowEmptyResults(false)
+		}
+
+	}
+}
