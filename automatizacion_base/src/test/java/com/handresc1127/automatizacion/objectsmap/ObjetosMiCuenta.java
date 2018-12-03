68946cdc7c5783694bf77b339319a018662e78e3
alasprilla <lasprilla.alexandra@gmail.com>
2018-12-03 14:24:43 -0500

    Se modifica el Xpath del campo de Linea Prueba Juanca

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/ObjetosMiCuenta.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/ObjetosMiCuenta.java
@@ -15,7 +15,7 @@ public class ObjetosMiCuenta {
 		ActionsUtil.objetosPut("continuarmicuenta", By.xpath("/html/body/div[2]/div/form/div[3]/button"));
 //		ActionsUtil.objetosPut("seleccionmsisdn", By.xpath("//*[@id='block-accountsblock-2']/div/div/div/div/div/span"));
 		ActionsUtil.objetosPut("seleccionmsisdn", By.xpath("//*[@id='block-accountsblock-2']/div/div/div/div/div/span|//*[@id='account-mobile-selector']/div[1]/span[2]"));
-		ActionsUtil.objetosPut("lineapruebasjuanca", By.xpath("(((//*[@id='lines'])//a[contains(@href,'')]))[11]|//*[@id='lines']/li[3]/div/div[2]/span[1]"));
+		ActionsUtil.objetosPut("lineapruebasjuanca", By.xpath("(((//*[@id='lines'])//a[contains(@href,'')]))[11]|(((((//*[@id='lines'])//a[contains(@href,'')])))//*[contains(text(),'')])[8]"));
 		ActionsUtil.objetosPut("lineahibridomariana", By.xpath("(((((//*[@id='lines'])//a[contains(@href,'')])))//*[contains(text(),'Mariana')])[2]|//*[@id='lines']/li[2]/a/div/div[2]"));
 		ActionsUtil.objetosPut("lineahibridoana", By.xpath("(((//*[@id='lines'])//a[contains(@href,'')]))[9]|//*[@id='lines']/li[1]/div/div[1]"));
 		ActionsUtil.objetosPut("usuario", By.xpath("//*[@id='top_menu_aside']/nav/ul/li/a"));
