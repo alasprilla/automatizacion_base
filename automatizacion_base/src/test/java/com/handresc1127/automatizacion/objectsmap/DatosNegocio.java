2b4128aa6fe77e9b69277007a8b2b51a8bba6f0b
hacorrea <hacorrea@indracompany.com>
2018-12-13 15:18:03 -0500

    Mejoras de calidad de codigo

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/DatosNegocio.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/objectsmap/DatosNegocio.java
@@ -7,8 +7,12 @@ import com.handresc1127.automatizacion.utilities.ActionsUtil;
 
 public final class DatosNegocio {
 	
+	private DatosNegocio() {
+	    throw new IllegalStateException("Utility class");
+	  }
 	
 	private static final HashMap<String, String> datosDelNegocio = new HashMap<>();
+	
 	static {
 		Calendar now = Calendar.getInstance();
 		dataPut("paginapagodefacturas", ActionsUtil.getProperty("baseURL1"));
@@ -25,7 +29,7 @@ public final class DatosNegocio {
 		now = Calendar.getInstance();
 		now.add(Calendar.YEAR, 1);
 		dataPut("nextyyyy()", String.valueOf(now.get(Calendar.YEAR)));
-	};
+	}
 	
 	public static String dataGet(String keyData) {
 		String retorno = datosDelNegocio.get(ActionsUtil.textoMinusculasSinEspacios(keyData));
