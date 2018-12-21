46d46e03cbb9c13d905f54e4c3185bc8b9756b65
hacorrea <hacorrea@indracompany.com>
2018-12-21 08:18:46 -0500

    Unit tests pasarela

--- unitTests/ejecucionUnitTests.sh
+++ unitTests/ejecucionUnitTests.sh
@@ -3,7 +3,7 @@
 echo ""
 echo ""
 echo "###############################################################################" 
-echo "############ Estableciendo permisos owner a Metis-WebReports.priv #############" 
+echo "##################### Copiando PhpUnit de transaccionales-co ##################" 
 echo "###############################################################################" 
 echo "####################################Éo=&I*?I±©#################################" 
 echo "##################################ë&&I%oI&&I=*?Ñ###############################" 
@@ -16,12 +16,12 @@ echo "##################################N@o£X£&%o£££É#####################
 echo "######################################æë£ë§N###################################" 
 echo "###############################################################################" 
 echo ""
-chmod 600 ../../Metis-WebReports.priv
+ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'cp metis\unitTests\phpunit_transaccionales.xml transaccionales-co/core/phpunit.xml'
 
 echo ""
 echo ""
 echo "###############################################################################" 
-echo "############################### Moviendo evidencias ###########################" 
+echo "################### Posicionandose en core de Transaccionales-co ##############" 
 echo "###############################################################################" 
 echo "####################################Éo=&I*?I±©#################################" 
 echo "##################################ë&&I%oI&&I=*?Ñ###############################" 
@@ -34,26 +34,13 @@ echo "##################################N@o£X£&%o£££É#####################
 echo "######################################æë£ë§N###################################" 
 echo "###############################################################################" 
 echo ""
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/5'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/5/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/4/* /var/www/metis/atdd/5/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/4'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/4/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/3/* /var/www/metis/atdd/4/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/3'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/3/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/2/* /var/www/metis/atdd/3/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/2'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/2/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/1/* /var/www/metis/atdd/2/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/1'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/1/*'
-chmod --recursive a+r /var/www/metis/atdd/*
+ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'cd transaccionales-co/core'
+
 
 echo ""
 echo ""
 echo "###############################################################################" 
-echo "########################## Publicando nuevas evidencias #######################" 
+echo "################ Ejecutando pruebas unitarias de transaccionales-co ###########" 
 echo "###############################################################################" 
 echo "####################################Éo=&I*?I±©#################################" 
 echo "##################################ë&&I%oI&&I=*?Ñ###############################" 
@@ -66,12 +53,13 @@ echo "##################################N@o£X£&%o£££É#####################
 echo "######################################æë£ë§N###################################" 
 echo "###############################################################################" 
 echo ""
-echo "put -r metis/automatizacion_base/target/site/serenity/* /var/www/metis/atdd/1/" | sftp -i ../../Metis-WebReports.priv metis@190.248.48.5
+ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'cd -'
+
 
 echo ""
 echo ""
 echo "###############################################################################" 
-echo "########### Formateando archivos de las nuevas evidencias (UTF-8) #############" 
+echo "#################### Posicionandose nuevamente en el workspace ################" 
 echo "###############################################################################" 
 echo "####################################Éo=&I*?I±©#################################" 
 echo "##################################ë&&I%oI&&I=*?Ñ###############################" 
@@ -84,4 +72,6 @@ echo "##################################N@o£X£&%o£££É#####################
 echo "######################################æë£ë§N###################################" 
 echo "###############################################################################" 
 echo ""
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'convmv -r -f cp1252 -t utf-8 /var/www/metis/atdd/* --notest > /dev/null'
+ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'cd -'
+
+exit 0
---
+++ unitTests/phpunit pasarela.groovy
@@ -0,0 +1,52 @@
+job("phpunit pasarela") {
+	description("Pruebas automatizadas proyecto de pasarela de pagos.")
+	keepDependencies(false)
+	multiscm {
+		git {
+			remote {
+				name('metis')
+				url('ssh://git@code.tigocloud.net/diffusion/137/metis.git')
+				credentials("e220c410-6ce3-4c0c-b7bc-1a14eeeb1d6c")
+				branch("*/master")
+              	extensions {
+              		relativeTargetDirectory('metis')
+              	}
+			}
+		}
+		git {
+			remote {
+				name('transaccionales-co')
+				url('ssh://git@code.tigocloud.net/diffusion/TRXCO/transaccionales-co.git')
+				credentials("e220c410-6ce3-4c0c-b7bc-1a14eeeb1d6c")
+				branch("*/master")
+              	extensions {
+              		relativeTargetDirectory('transaccionales-co')
+              	}
+			}
+		}
+		git {
+			remote {
+				name('tigoselfcareregional')
+				url('ssh://git@code.tigocloud.net/diffusion/84/tigoselfcareregional.git')
+				credentials("e220c410-6ce3-4c0c-b7bc-1a14eeeb1d6c")
+				branch("*/master")
+              	extensions {
+              		relativeTargetDirectory('tigoselfcareregional')
+              	}
+			}
+		}
+	}
+	disabled(false)
+    triggers {
+          githubPush()
+      }
+
+	concurrentBuild(false)
+
+
+	steps {
+      shell('chmod a+x metis/unitTests/ejecucionUnitTests.sh')
+      shell('metis/unitTests/ejecucionUnitTests.sh')
+    }
+	
+}
