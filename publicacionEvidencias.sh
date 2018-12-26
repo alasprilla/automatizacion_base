4b2f80877329be7468404fda539c9b03c731727a
hacorrea <hacorrea@indracompany.com>
2018-12-26 09:10:50 -0500

    Llave de metis

--- publicacionEvidencias.sh
+++ publicacionEvidencias.sh
@@ -3,24 +3,6 @@
 echo ""
 echo ""
 echo "###############################################################################" 
-echo "############ Estableciendo permisos owner a Metis-WebReports.priv #############" 
-echo "###############################################################################" 
-echo "####################################Éo=&I*?I±©#################################" 
-echo "##################################ë&&I%oI&&I=*?Ñ###############################" 
-echo "#################################±I&X£±X±£%*=&*%ë##############################" 
-echo "################################Ño%X£ë@±ëX£%££&*o¶#############################" 
-echo "################################XoX±£@X£Xo±XXXoIoæ#############################" 
-echo "################################§&XëÑÑÑ±£o%%&£±oI##############################" 
-echo "#################################@&±Ñ§ÑÑ£oo±ë@Ñ£N##############################" 
-echo "##################################N@o£X£&%o£££É################################" 
-echo "######################################æë£ë§N###################################" 
-echo "###############################################################################" 
-echo ""
-chmod 600 ../../Metis-WebReports.priv
-
-echo ""
-echo ""
-echo "###############################################################################" 
 echo "############################### Moviendo evidencias ###########################" 
 echo "###############################################################################" 
 echo "####################################Éo=&I*?I±©#################################" 
@@ -34,21 +16,21 @@ echo "##################################N@o£X£&%o£££É#####################
 echo "######################################æë£ë§N###################################" 
 echo "###############################################################################" 
 echo ""
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/5'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/5/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/4/* /var/www/metis/atdd/5/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/4'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/4/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/3/* /var/www/metis/atdd/4/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/3'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/3/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/2/* /var/www/metis/atdd/3/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/2'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/2/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mv /var/www/metis/atdd/1/* /var/www/metis/atdd/2/'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/1'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'rm -rf /var/www/metis/atdd/1/*'
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'chmod --recursive a+r /var/www/metis/atdd/*'
+ssh metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/5'
+ssh metis@190.248.48.5 'rm -rf /var/www/metis/atdd/5/*'
+ssh metis@190.248.48.5 'mv /var/www/metis/atdd/4/* /var/www/metis/atdd/5/'
+ssh metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/4'
+ssh metis@190.248.48.5 'rm -rf /var/www/metis/atdd/4/*'
+ssh metis@190.248.48.5 'mv /var/www/metis/atdd/3/* /var/www/metis/atdd/4/'
+ssh metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/3'
+ssh metis@190.248.48.5 'rm -rf /var/www/metis/atdd/3/*'
+ssh metis@190.248.48.5 'mv /var/www/metis/atdd/2/* /var/www/metis/atdd/3/'
+ssh metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/2'
+ssh metis@190.248.48.5 'rm -rf /var/www/metis/atdd/2/*'
+ssh metis@190.248.48.5 'mv /var/www/metis/atdd/1/* /var/www/metis/atdd/2/'
+ssh metis@190.248.48.5 'mkdir -p /var/www/metis/atdd/1'
+ssh metis@190.248.48.5 'rm -rf /var/www/metis/atdd/1/*'
+ssh metis@190.248.48.5 'chmod --recursive a+r /var/www/metis/atdd/*'
 
 echo ""
 echo ""
@@ -66,7 +48,7 @@ echo "##################################N@o£X£&%o£££É#####################
 echo "######################################æë£ë§N###################################" 
 echo "###############################################################################" 
 echo ""
-echo "put -r metis/automatizacion_base/target/site/serenity/* /var/www/metis/atdd/1/" | sftp -i ../../Metis-WebReports.priv metis@190.248.48.5
+echo "put -r metis/automatizacion_base/target/site/serenity/* /var/www/metis/atdd/1/" | sftp metis@190.248.48.5
 
 echo ""
 echo ""
@@ -84,4 +66,4 @@ echo "##################################N@o£X£&%o£££É#####################
 echo "######################################æë£ë§N###################################" 
 echo "###############################################################################" 
 echo ""
-ssh -i ../../Metis-WebReports.priv metis@190.248.48.5 'convmv -r -f cp1252 -t utf-8 /var/www/metis/atdd/* --notest > /dev/null'
+ssh metis@190.248.48.5 'convmv -r -f cp1252 -t utf-8 /var/www/metis/atdd/* --notest > /dev/null'
