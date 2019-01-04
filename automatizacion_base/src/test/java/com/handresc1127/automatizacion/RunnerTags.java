2a6f2989b3022fc0f58e50b8675527939866c8b8
hacorrea <hacorrea@indracompany.com>
2019-01-04 12:15:43 -0500

    Ajustes debug Runner HU025

--- automatizacion_base/src/test/java/com/handresc1127/automatizacion/RunnerTags.java
+++ automatizacion_base/src/test/java/com/handresc1127/automatizacion/RunnerTags.java
@@ -60,9 +60,10 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 		"src/test/resources/features/ADP-109_HU-024_DetallesServiciosFijos.feature",
 		"src/test/resources/features/ADP-101_PruebaAdherenciaDePaquetes.feature",
 		"src/test/resources/features/ADP-108_HU-023_CambiarDMZ.feature",
+		"src/test/resources/features/ADP-110_HU-025_HistoricoConsumosTelefonia.feature",
 		"src/test/resources/features/ADP-111_HU-026_Consulta de PQR.feature" },
 
-		tags = { "@issue:ADP-111" }, monochrome = true, plugin = { "pretty", "html:target/reports/html/",
+		tags = { "@issue:ADP-110" }, monochrome = true, plugin = { "pretty", "html:target/reports/html/",
 				"junit:target/reports/junit.xml", "json:target/reports/cukes.json" })
 
 public class RunnerTags {
